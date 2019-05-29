9a1a0666f8c2512bd50cf12560766c19128f9236
Your Name <sharan20051998@gmail.com>
2019-05-29 15:48:40 +0530

    4

--- app/entrypoint.sh
+++ app/entrypoint.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-python3 manage.py runserver 0.0.0.0:8000 &
-
-cd "python-flask-server-generated"
-cd "python-flask-server"
-ls
-python3 -m swagger_server
+python3 manage.py runserver 0.0.0.0:8000
